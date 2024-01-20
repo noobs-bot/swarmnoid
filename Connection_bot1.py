1df02fb7d3d80a50415e2796a9ee99e3785b259a
Ramu-acharya <Ramuacharyarupandehi@gmail.com>
2024-01-21 03:55:27 +0545

    Connection_bot1

--- Connection_bot1.py
+++ Connection_bot1.py
@@ -6,7 +6,7 @@ import socket
 
 # Set the IP address and port on which the laptop server will listen
 host = '0.0.0.0'  # Listen on all available interfaces
-port = 12345
+port = 1111
 
 # Create a socket object
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
